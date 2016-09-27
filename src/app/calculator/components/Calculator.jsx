fb4a7a5849773b183e5a0ce8238d7c1d747338bf
Sylphony <symphonictwist@gmail.com>
2016-09-27 03:54:48 -0700

    Moved event callback to separate file; placed handler inside constructor to allow named function event removal

--- src/app/calculator/components/Calculator.jsx
+++ src/app/calculator/components/Calculator.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 import CalculatorButton from "./CalculatorButton.jsx";
 import CalculatorScreen from "./CalculatorScreen.jsx";
+import calculatorKeyboard from "./functions/calculatorKeyboard";
 
 class Calculator extends React.Component {
+    constructor(props) {
+        super(props);
+
+        // Handler for keyboard event listener
+        this.calculatorKeyboardHandler = function(e) {
+            calculatorKeyboard(e, props);
+        };
+    }
+
     render() {
         return (
             <div className="calculator">
@@ -42,69 +52,11 @@ class Calculator extends React.Component {
     }
 
     componentDidMount() {
-        // A lookup object to see which key type is pressed
-        const keyLookup = {
-            num: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
-            decimal: ["."],
-            operation: ["+", "-", "*", "/"],
-            equal: ["=", "Enter"],
-            toggleSign: ["F9"],
-            backspace: ["Backspace"],
-            clear: ["Escape"]
-        };
-
-        window.addEventListener("keydown", (e) => {
-            e.preventDefault(); // Stop any events that may be triggered as a result of button focus (e.g. pressing enter while button is focused from clicking)
-
-            let keyChar = e.key;
-
-            // If ESC is pressed
-            if (keyLookup.clear.indexOf(keyChar) > -1) {
-                this.props.actions.pressClear();
-            }
-
-            // Only allow input as long the calculator has not run into an error
-            if (!this.props.calculator.disableInput) {
-
-                // If it is a number key
-                if (keyLookup.num.indexOf(keyChar) > -1) {
-                    this.props.actions.pressNum(keyChar);
-                }
-
-                // If it is a decimal key
-                else if (keyLookup.decimal.indexOf(keyChar) > -1) {
-                    this.props.actions.pressDecimal();
-                }
-
-                // If it is the F9 key (for toggle sign)
-                else if (keyLookup.toggleSign.indexOf(keyChar) > -1) {
-                    this.props.actions.pressToggleSign();
-                }
-
-                // If it is an operation key
-                else if (keyLookup.operation.indexOf(keyChar) > -1) {
-                    // Convert multi/div keys to usage variant
-                    if (keyChar === "*") keyChar = "×";
-                    if (keyChar === "/") keyChar = "÷";
-
-                    this.props.actions.pressOperation(keyChar);
-                }
-
-                // If equal sign or enter key
-                else if (keyLookup.equal.indexOf(keyChar) > -1) {
-                    this.props.actions.pressEqual();
-                }
-
-                // If backspace key is pressed
-                else if (keyLookup.backspace.indexOf(keyChar) > -1) {
-                    this.props.actions.pressBackspace();
-                }
-            }
-        });
+        window.addEventListener("keydown", this.calculatorKeyboardHandler);
     }
 
     componentWillUnmount() {
-        window.removeEventListener("keydown");
+        window.removeEventListener("keydown", this.calculatorKeyboardHandler);
     }
 }
 
---
+++ src/app/calculator/components/functions/calculatorKeyboard.js
@@ -0,0 +1,66 @@
+// A lookup object to see which key type is pressed
+const keyLookup = {
+    num: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
+    decimal: ["."],
+    operation: ["+", "-", "*", "/"],
+    equal: ["=", "Enter"],
+    toggleSign: ["F9"],
+    backspace: ["Backspace"],
+    clear: ["Escape"]
+};
+
+/**
+ * Keyboard event callback for calculator.
+ * @param e: Event (keydown).
+ * @param props: The component's properties.
+ */
+function calculatorKeyboard(e, props) {
+    // Stop any events that may be triggered as a result of button focus (e.g. pressing enter while button is focused from clicking)
+    e.preventDefault();
+
+    let keyChar = e.key;
+
+    // If ESC is pressed
+    if (keyLookup.clear.indexOf(keyChar) > -1) {
+        props.actions.pressClear();
+    }
+
+    // Only allow input as long the calculator has not run into an error
+    if (!props.calculator.disableInput) {
+        // If it is a number key
+        if (keyLookup.num.indexOf(keyChar) > -1) {
+            props.actions.pressNum(keyChar);
+        }
+
+        // If it is a decimal key
+        else if (keyLookup.decimal.indexOf(keyChar) > -1) {
+            props.actions.pressDecimal();
+        }
+
+        // If it is the F9 key (for toggle sign)
+        else if (keyLookup.toggleSign.indexOf(keyChar) > -1) {
+            props.actions.pressToggleSign();
+        }
+
+        // If it is an operation key
+        else if (keyLookup.operation.indexOf(keyChar) > -1) {
+            // Convert multi/div keys to usage variant
+            if (keyChar === "*") keyChar = "×";
+            if (keyChar === "/") keyChar = "÷";
+
+            props.actions.pressOperation(keyChar);
+        }
+
+        // If equal sign or enter key
+        else if (keyLookup.equal.indexOf(keyChar) > -1) {
+            props.actions.pressEqual();
+        }
+
+        // If backspace key is pressed
+        else if (keyLookup.backspace.indexOf(keyChar) > -1) {
+            props.actions.pressBackspace();
+        }
+    }
+}
+
+export default calculatorKeyboard;
