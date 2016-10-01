dd2094c1c14d38b9fdddcfb75ecfe860afef7d7c
Sylphony <symphonictwist@gmail.com>
2016-10-01 01:42:22 -0700

    Switched to shallow rendering via enzyme

--- src/app/commands-list/components/CommandsListButtonToggle.spec.jsx
+++ src/app/commands-list/components/CommandsListButtonToggle.spec.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import TestUtils from "react-addons-test-utils";
 import CommandsListButtonToggle from "./CommandsListButtonToggle.jsx";
+import { shallow, mount, render } from "enzyme";
 
 describe("CommandsListButtonToggle component", function() {
     beforeEach(function() {
@@ -8,15 +9,25 @@ describe("CommandsListButtonToggle component", function() {
             console.log("CommandsListButtonToggle event");
         };
 
-        this.component = TestUtils.renderIntoDocument(<CommandsListButtonToggle click={ clickFunc } />);
+        this.wrapper = shallow(<CommandsListButtonToggle click={ clickFunc } />);
     });
 
-    it("has all button props", function() {
-        let button = TestUtils.findRenderedDOMComponentWithTag(this.component, "button");
+    it("has a single button element", function() {
+        let btnEle = this.wrapper.find(".commands-list__btn-toggle");
 
-        // TestUtils.Simulate.click(button);
+        expect(btnEle.length).toBe(1);
+        expect(btnEle.is("button")).toBe(true);
+    });
 
-        expect(button.type).toBe("button");
-        expect(button.className).toBe("commands-list__btn-toggle button");
+    it("has a single icon element", function() {
+        let iconEle = this.wrapper.find(".commands-list__btn-toggle-icon");
+
+        expect(iconEle.length).toBe(1);
+    });
+
+    it("has a text label", function() {
+        let textEle = this.wrapper.find(".commands-list__btn-toggle-text");
+
+        expect(textEle.length).toBe(1);
     });
 });
