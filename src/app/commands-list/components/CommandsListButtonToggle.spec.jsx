cf0a9d1336c920fb48b15fbab0075c050f5e5747
Sylphony <symphonictwist@gmail.com>
2016-10-01 21:19:32 -0700

    Added simulate click button test

--- src/app/commands-list/components/CommandsListButtonToggle.spec.jsx
+++ src/app/commands-list/components/CommandsListButtonToggle.spec.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 import TestUtils from "react-addons-test-utils";
 import CommandsListButtonToggle from "./CommandsListButtonToggle.jsx";
-import { shallow, mount, render } from "enzyme";
+import { shallow } from "enzyme";
 
 describe("CommandsListButtonToggle component", function() {
     beforeEach(function() {
-        let clickFunc = () => {
-            console.log("CommandsListButtonToggle event");
+        let mockEvents = {
+            clickFunc: function() {
+                this.counter++;
+            }
         };
 
-        this.wrapper = shallow(<CommandsListButtonToggle click={ clickFunc } />);
+        // Watch over the click function for later simulation calls
+        spyOn(mockEvents, "clickFunc");
+
+        this.wrapper = shallow(<CommandsListButtonToggle click={ mockEvents.clickFunc } />);
     });
 
     it("has a single button element", function() {
@@ -23,11 +28,19 @@ describe("CommandsListButtonToggle component", function() {
         let iconEle = this.wrapper.find(".commands-list__btn-toggle-icon");
 
         expect(iconEle.length).toBe(1);
+        expect(iconEle.hasClass("fa")).toBe(true);
     });
 
     it("has a text label", function() {
         let textEle = this.wrapper.find(".commands-list__btn-toggle-text");
 
         expect(textEle.length).toBe(1);
+    });
+
+    it("has a working click event on button", function() {
+        let btnEle = this.wrapper.find(".commands-list__btn-toggle");
+
+        btnEle.simulate("click");
+        expect(btnEle.prop("onClick")).toHaveBeenCalled();
     });
 });
